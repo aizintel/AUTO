7969e13a789061cde70b2c0447a5723551b05008
intel.aiz <148807482+aizintel@users.noreply.github.com>
2024-01-30 01:33:25 -0800

    Update script.js

--- public/script.js
+++ public/script.js
@@ -1,80 +1,156 @@
 document.getElementById('agreeCheckbox').addEventListener('change', function() {
-   document.getElementById('submitButton').disabled = !this.checked;
+  document.getElementById('submitButton').disabled = !this.checked;
 });
+let Commands = [{
+  'commands': []
+}, {
+  'handleEvent': []
+}];
 
 function measurePing() {
-   var xhr = new XMLHttpRequest();
-   var startTime, endTime;
-   xhr.onreadystatechange = function() {
-      if (xhr.readyState === 4) {
-         endTime = Date.now();
-         var pingTime = endTime - startTime;
-         document.getElementById("ping").textContent = pingTime + " ms";
-      }
-   };
-   xhr.open("GET", location.href + "?t=" + new Date().getTime());
-   startTime = Date.now();
-   xhr.send();
+  var xhr = new XMLHttpRequest();
+  var startTime, endTime;
+  xhr.onreadystatechange = function() {
+    if (xhr.readyState === 4) {
+      endTime = Date.now();
+      var pingTime = endTime - startTime;
+      document.getElementById("ping").textContent = pingTime + " ms";
+    }
+  };
+  xhr.open("GET", location.href + "?t=" + new Date().getTime());
+  startTime = Date.now();
+  xhr.send();
 }
 setInterval(measurePing, 1000);
 
 function updateTime() {
-   const now = new Date();
-   const options = {
-      timeZone: 'Asia/Manila',
-      hour12: true,
-      hour: 'numeric',
-      minute: 'numeric',
-      second: 'numeric'
-   };
-   const formattedTime = now.toLocaleString('en-US', options);
-   document.getElementById('time').textContent = formattedTime;
+  const now = new Date();
+  const options = {
+    timeZone: 'Asia/Manila',
+    hour12: true,
+    hour: 'numeric',
+    minute: 'numeric',
+    second: 'numeric'
+  };
+  const formattedTime = now.toLocaleString('en-US', options);
+  document.getElementById('time').textContent = formattedTime;
 }
 updateTime();
 setInterval(updateTime, 1000);
 async function State() {
-   const jsonInput = document.getElementById('json-data');
-   const button = document.getElementById('submitButton');
-   try {
-      button.style.display = 'none'; 
-      const State = JSON.parse(jsonInput.value);
-      if (State && typeof State === 'object') {
-         const response = await fetch('/login', {
-            method: 'POST',
-            headers: {
-               'Content-Type': 'application/json',
-            },
-            body: JSON.stringify({
-               state: State
-            }),
-         });
-         const data = await response.json();
-         if (data.success) {
-            jsonInput.value = '';
-            showResult(data.message);
-         } else {
-            jsonInput.value = '';
-            showResult(data.message);
-         }
+  const jsonInput = document.getElementById('json-data');
+  const button = document.getElementById('submitButton');
+  try {
+    button.style.display = 'none';
+    const State = JSON.parse(jsonInput.value);
+    if (State && typeof State === 'object') {
+      const response = await fetch('/login', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          state: State,
+          commands: Commands,
+        }),
+      });
+      const data = await response.json();
+      if (data.success) {
+        jsonInput.value = '';
+        showResult(data.message);
       } else {
-         jsonInput.value = '';
-         showResult('Invalid JSON data. Please check your input.');
+        jsonInput.value = '';
+        showResult(data.message);
       }
-   } catch (parseError) {
+    } else {
       jsonInput.value = '';
-      console.error('Error parsing JSON:', parseError);
-      showResult('Error parsing JSON. Please check your input.');
-   } finally {
-      setTimeout(() => {
-         button.style.display = 'block';
-      }, 4000);
-   }
+      showResult('Invalid JSON data. Please check your input.');
+    }
+  } catch (parseError) {
+    jsonInput.value = '';
+    console.error('Error parsing JSON:', parseError);
+    showResult('Error parsing JSON. Please check your input.');
+  } finally {
+    setTimeout(() => {
+      button.style.display = 'block';
+    }, 4000);
+  }
 }
 
 function showResult(message) {
-   const resultContainer = document.getElementById('result');
-   resultContainer.innerHTML = `<h5>${message}</h5>`;
-   resultContainer.style.display = 'block';
+  const resultContainer = document.getElementById('result');
+  resultContainer.innerHTML = `<h5>${message}</h5>`;
+  resultContainer.style.display = 'block';
+}
+async function commandList() {
+  try {
+    const [listOfCommands, listOfCommandsEvent] = [document.getElementById('listOfCommands'), document.getElementById('listOfCommandsEvent')];
+    const response = await fetch('/commands');
+    const {
+      commands,
+      handleEvent
+    } = await response.json();
+    [commands, handleEvent].forEach((commandArray, i) => {
+      commandArray.forEach((command, index) => {
+        const container = createCommand(i === 0 ? listOfCommands : listOfCommandsEvent, index + 1, command, i === 0 ? 'commands' : 'handleEvent');
+        i === 0 ? listOfCommands.appendChild(container) : listOfCommandsEvent.appendChild(container);
+      });
+    });
+  } catch (error) {
+    console.log(error);
+  }
 }
 
+function createCommand(parentElement, order, command, type) {
+  const container = document.createElement('div');
+  container.classList.add('form-check', 'form-switch');
+  container.onclick = toggleCheckbox;
+  const checkbox = document.createElement('input');
+  checkbox.classList.add('form-check-input', type === 'handleEvent' ? 'handleEvent' : 'commands');
+  checkbox.type = 'checkbox';
+  checkbox.role = 'switch';
+  checkbox.id = `flexSwitchCheck_${order}`;
+  const label = document.createElement('label');
+  label.classList.add('form-check-label', type === 'handleEvent' ? 'handleEvent' : 'commands');
+  label.for = `flexSwitchCheck_${order}`;
+  label.textContent = `${order}. ${command}`;
+  container.appendChild(checkbox);
+  container.appendChild(label);
+  return container;
+}
 
+function toggleCheckbox() {
+  const checkboxMap = [{
+    input: '.form-check-input.commands',
+    label: '.form-check-label.commands',
+    array: Commands[0].commands
+  }, {
+    input: '.form-check-input.handleEvent',
+    label: '.form-check-label.handleEvent',
+    array: Commands[1].handleEvent
+  }];
+  checkboxMap.forEach(({
+    input,
+    label,
+    array
+  }) => {
+    const checkbox = this.querySelector(input);
+    const labelText = this.querySelector(label);
+    if (checkbox) {
+      checkbox.checked = !checkbox.checked;
+      if (checkbox.checked) {
+        labelText.classList.add('disable');
+        const command = labelText.textContent.replace(/^\d+\.\s/, '');
+        array.push(command);
+      } else {
+        labelText.classList.remove('disable');
+        const command = labelText.textContent.replace(/^\d+\.\s/, '');
+        const removeCommand = array.indexOf(command);
+        if (removeCommand !== -1) {
+          array.splice(removeCommand, 1);
+        }
+      }
+    }
+  });
+}
+commandList();
