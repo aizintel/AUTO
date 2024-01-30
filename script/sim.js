5be664dbd9581a64f4cce4a64b2513b6033f9210
intel.aiz <148807482+aizintel@users.noreply.github.com>
2024-01-30 01:34:05 -0800

    Create sim.js

---
+++ script/sim.js
@@ -0,0 +1,36 @@
+module.exports.config = {
+  name: "sim",
+  version: "1.0.0",
+};
+module.exports.run = async function({
+  api,
+  event,
+  args
+}) {
+  const axios = require("axios");
+  let {
+    messageID,
+    threadID,
+    senderID,
+    body
+  } = event;
+  let tid = threadID,
+    mid = messageID;
+  const content = encodeURIComponent(args.join(" "));
+  if (!args[0]) return api.sendMessage("Usage: sim [ text ]", tid, mid);
+  try {
+    const res = await axios.get(`https://simsimi.fun/api/v2/?mode=talk&lang=ph&message=${content}&filter=false`);
+    const respond = res.data.success;
+    if (res.data.error) {
+      api.sendMessage(`Error please try again later.`, tid, (error, info) => {
+        if (error) {}
+      }, mid);
+    } else {
+      api.sendMessage(respond, tid, (error, info) => {
+        if (error) {}
+      }, mid);
+    }
+  } catch (error) {
+    api.sendMessage("An error occurred while fetching the data.", tid, mid);
+  }
+};
