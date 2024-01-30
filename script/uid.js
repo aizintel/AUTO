332aaaaa05dbec1ea498cef9d5bbb9a41aac4e8a
intel.aiz <148807482+aizintel@users.noreply.github.com>
2024-01-30 18:10:57 +0800

    Update uid.js

--- script/uid.js
+++ script/uid.js
@@ -10,6 +10,6 @@ module.exports.run = async function({
   if (Object.keys(event.mentions) == 0) { api.sendMessage(`${event.senderID}`, event.threadID, event.messageID) }
   else {
     for (var i = 0; i < Object.keys(event.mentions).length; i++) api.sendMessage(`${Object.values(event.mentions)[i].replace('@', '')}: ${Object.keys(event.mentions)[i]}`, event.threadID);
-    
+    return;
   }
 }
