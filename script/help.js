7a34d30a2dabdee7b3bfb8dd31f2018cd98d880f
ashhhey <157706699+ashhhey@users.noreply.github.com>
2024-02-06 23:55:33 +0800

    Update help.js

--- script/help.js
+++ script/help.js
@@ -1,36 +1,49 @@
 module.exports.config = {
   name: 'help',
   version: '1.0.0',
-  role: 0,
-  aliases: ['info']
 };
-module.exports.run = async function({
-  api,
-  event,
-  enableCommands,
-  args
-}) {
+
+module.exports.run = async function ({ api, event, enableCommands, args }) {
   const input = args.join(' ');
+
   try {
-    const eventCommands = enableCommands[1].handleEvent;
-    const commands = enableCommands[0].commands;
-    const commandsPerPage = 20;
-    const page = input ? parseInt(input) : 1;
-    let startIdx = (page - 1) * commandsPerPage;
-    let endIdx = startIdx + commandsPerPage;
-    let helpMessage = 'Commands:\n\n';
-    for (let i = startIdx; i < Math.min(endIdx, commands.length); i++) {
-      helpMessage += `\t${i + 1}. ${commands[i]}\n`;
-    }
-    helpMessage += '\nEvent:\n\n';
-    eventCommands.forEach((eventCommand, index) => {
-      helpMessage += `\t${index + 1}. ${eventCommand}\n`;
-    });
-    if (commands.length > endIdx) {
-      helpMessage += `\nPage ${page} - To access the next page, use: help ${page + 1}`;
-    }
-    api.sendMessage(helpMessage, event.threadID, event.messageID);
-  } catch (error) {
-    console.log(error)
+    
+  const eventCommands = enableCommands[1].handleEvent;
+  const commands = enableCommands[0].commands;
+
+  const commandsPerPage = 20;
+  const page = input ? parseInt(input) : 1;
+
+  let startIdx = (page - 1) * commandsPerPage;
+  let endIdx = startIdx + commandsPerPage;
+  let time = process.uptime();
+  let hours = Math.floor(time / (60 * 60));
+  let minutes = Math.floor((time % (60 * 60)) / 60);
+  let seconds = Math.floor(time % 60);
+  const timeStart = Date.now();
+
+  const hoursString = hours === 1 ? "hour" : "hours";
+      const minutesString = minutes === 1 ? "minute" : "minutes";
+      const secondsString = seconds === 1 ? "second" : "seconds";
+
+      const uptimeString = `${hours > 0 ? `${hours} ` : ''} : ${minutes > 0 ? `${minutes} ` : ''} : ${seconds} `;
+
+  let helpMessage = '𝗔𝘃𝗮𝗶𝗹𝗮𝗯𝗹𝗲 𝗖𝗼𝗺𝗺𝗮𝗻𝗱𝘀:\n\n';
+  for (let i = startIdx; i < Math.min(endIdx, commands.length); i++) {
+    helpMessage += `${i + 1}. ${commands[i]}\n`;
   }
+
+  helpMessage += '\n𝗔𝘃𝗮𝗶𝗹𝗮𝗯𝗹𝗲 𝗘𝘃𝗲𝗻𝘁𝘀:\n\n';
+  eventCommands.forEach((eventCommand, index) => {
+    helpMessage += `${index + 1}. ${eventCommand}\n`;
+  });
+
+  if (commands.length > endIdx) {
+    helpMessage += `\n━━━━━━━━━━━━━━━━━━\n📈𝗣𝗜𝗡𝗚: ${(Date.now() - timeStart)}ms\n🕑𝗨𝗣𝗧𝗜𝗠𝗘: ${uptimeString}`;
+  }
+
+  api.sendMessage(`${helpMessage}`, event.threadID, event.messageID);
+    } catch (error) {
+      console.log(error)
+    }
 };
