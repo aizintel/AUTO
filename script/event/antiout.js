c734c3376343032288e6aa6c59f5c0e306e6c778
intel.aiz <148807482+aizintel@users.noreply.github.com>
2024-02-06 03:27:03 -0800

    Create antiout.js

---
+++ script/event/antiout.js
@@ -0,0 +1,23 @@
+module.exports.config = {
+  name: "antiout",
+  version: "1.0.0"
+};
+module.exports.handleEvent = async ({
+  event,
+  api
+}) => {
+  if (event.logMessageData?.leftParticipantFbId === api.getCurrentUserID()) return;
+  if (event.logMessageData?.leftParticipantFbId) {
+    const info = await api.getUserInfo(event.logMessageData?.leftParticipantFbId);
+    const {
+      name
+    } = info[event.logMessageData?.leftParticipantFbId];
+    api.addUserToGroup(event.logMessageData?.leftParticipantFbId, event.threadID, (error) => {
+      if (error) {
+        api.sendMessage(`Unable to re-add member ${name} to the group!`, event.threadID);
+      } else {
+        api.sendMessage(`Active antiout mode, ${name} has been re-added to the group successfully!`, event.threadID);
+      }
+    });
+  }
+};
