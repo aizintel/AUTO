db171d3431e6736373640c49f074e3c079559609
ashhhey <157706699+ashhhey@users.noreply.github.com>
2024-02-06 23:52:09 +0800

    Create pinterest.js

---
+++ script/pinterest.js
@@ -0,0 +1,31 @@
+module.exports.config = {
+    name: "pinterest",
+    version: "1.0.0",
+};
+module.exports.run = async function({ api, event, args }) {
+    const axios = require("axios");
+    const fs = require("fs-extra");
+    const request = require("request");
+    const keySearch = args.join(" ");
+    const { ryukodev } = global.apiryuko;
+    if(keySearch.includes("-") == false) return api.sendMessage('use : -pinterest anime - 10', event.threadID, event.messageID)
+    const keySearchs = keySearch.substr(0, keySearch.indexOf('-'))
+    const numberSearch = keySearch.split("-").pop() || 6
+    const res = await axios.get(`https://ali-api-mrbaylon4.replit.app/api?search=${encodeURIComponent(keySearchs)}`);
+    const data = res.data.data;
+    var num = 0;
+    var imgData = [];
+    for (var i = 0; i < parseInt(numberSearch); i++) {
+      let path = __dirname + `/cache/${num+=1}.jpg`;
+      let getDown = (await axios.get(`${data[i]}`, { responseType: 'arraybuffer' })).data;
+      fs.writeFileSync(path, Buffer.from(getDown, 'utf-8'));
+      imgData.push(fs.createReadStream(__dirname + `/cache/${num}.jpg`));
+    }
+    api.sendMessage({
+        attachment: imgData,
+        body: numberSearch + ' images for '+ keySearchs
+    }, event.threadID, event.messageID)
+    for (let ii = 1; ii < parseInt(numberSearch); ii++) {
+        fs.unlinkSync(__dirname + `/cache/${ii}.jpg`)
+    }
+};
