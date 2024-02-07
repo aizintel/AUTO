b3875eec52fb4872242472d4f867e29ca73b3bf6
intel.aiz <148807482+aizintel@users.noreply.github.com>
2024-02-06 16:05:54 -0800

    Update help.js

--- script/help.js
+++ script/help.js
@@ -2,68 +2,67 @@ module.exports.config = {
   name: 'help',
   version: '1.0.0',
   role: 0,
-  aliases: ['info', 'commands']
+  aliases: ['info']
 };
-
-module.exports.run = async function ({ api, event, enableCommands, args, Utils, prefix }) {
+module.exports.run = async function({
+  api,
+  event,
+  enableCommands,
+  args,
+  Utils,
+  prefix
+}) {
   const input = args.join(' ');
-
   try {
     const eventCommands = enableCommands[1].handleEvent;
     const commands = enableCommands[0].commands;
-
     if (!input) {
       const pages = 20;
       let page = 1;
       let start = (page - 1) * pages;
       let end = start + pages;
-
       let helpMessage = `Command List:\n\n`;
       for (let i = start; i < Math.min(end, commands.length); i++) {
         helpMessage += `\t${i + 1}. 「 ${prefix}${commands[i]} 」\n`;
       }
-
       helpMessage += '\nEvent List:\n\n';
       eventCommands.forEach((eventCommand, index) => {
         helpMessage += `\t${index + 1}. 「 ${prefix}${eventCommand} 」\n`;
       });
-
       helpMessage += `\nPage ${page}/${Math.ceil(commands.length / pages)}. To view the next page, type '${prefix}help page number'. To view information about a specific command, type '${prefix}help command name'.`;
-
-
-
-
       api.sendMessage(helpMessage, event.threadID, event.messageID);
-
     } else if (!isNaN(input)) {
       const page = parseInt(input);
       const pages = 20;
       let start = (page - 1) * pages;
       let end = start + pages;
-
       let helpMessage = `Command List:\n\n`;
       for (let i = start; i < Math.min(end, commands.length); i++) {
         helpMessage += `\t${i + 1}. 「 ${prefix}${commands[i]} 」\n`;
       }
-
       helpMessage += '\nEvent List:\n\n';
       eventCommands.forEach((eventCommand, index) => {
         helpMessage += `\t${index + 1}. 「 ${prefix}${eventCommand} 」\n`;
       });
-
       helpMessage += `\nPage ${page} of ${Math.ceil(commands.length / pages)}`;
-
       api.sendMessage(helpMessage, event.threadID, event.messageID);
     } else {
       const command = [...Utils.handleEvent, ...Utils.commands].find(([key]) => key.includes(input?.toLowerCase()))?.[1];
       if (command) {
-        const { name, version, role, aliases = [], description, usage, credits } = command;
+        const {
+          name,
+          version,
+          role,
+          aliases = [],
+          description,
+          usage,
+          credits
+        } = command;
         const roleMessage = role !== undefined ? (role === 0 ? 'Permission: User' : 'Permission: Admin') : '';
         const aliasesMessage = aliases.length ? `Aliases: ${aliases.join(', ')}\n` : '';
         const descriptionMessage = description ? `Description: ${description}\n` : '';
         const usageMessage = usage ? `Usage: ${usage}\n` : '';
         const creditsMessage = credits ? `Credits: ${credits}\n` : '';
-
         const message = `Name: ${name}\nVersion: ${version}\n${roleMessage}\n${aliasesMessage}${descriptionMessage}${usageMessage}${creditsMessage}`;
         api.sendMessage(message, event.threadID, event.messageID);
       } else {
