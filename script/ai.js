bd0840cbe01a58af74f77dd2c98749b6d1a79848
Ulric-aty <160679258+Ulric-aty@users.noreply.github.com>
2024-03-06 20:51:48 +0100

    Update ai.js

--- script/ai.js
+++ script/ai.js
@@ -17,16 +17,16 @@ module.exports.run = async function({
 }) {
   const input = args.join(' ');
   if (!input) {
-    api.sendMessage(`Salut! je suis votre assistant virtuel.Comment puis je vous aider aujourd'hui ?`, event.threadID, event.messageID);
+    api.sendMessage(`Salut!je suis votre assistant virtuel.Comment puis je vous aider aujourd'hui?`, event.threadID, event.messageID);
     return;
   }
-  api.sendMessage(`Recherche🔍 "${input}"`, event.threadID, event.messageID);
+  api.sendMessage(`🔍"${input}"`, event.threadID, event.messageID);
   try {
     const {
       data
     } = await axios.get(`https://openaikey-x20f.onrender.com/api?prompt=${encodeURIComponent(input)}`);
     const response = data.response;
-    api.sendMessage(`|Ulric-Bot projet|, response, event.threadID, event.messageID);
+    api.sendMessage(`response`, event.threadID, event.messageID);
   } catch (error) {
     api.sendMessage('Une erreur est survenue lors de la recherche d'informations...', event.threadID, event.messageID);
   }
