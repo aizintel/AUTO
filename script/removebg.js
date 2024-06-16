0f66514e89352639eec4cde852d68277dfc91519
Maoribot <k1mazumi+705@yandex.com>
2024-06-17 06:57:19 +0800

    Update removebg.js

--- script/removebg.js
+++ script/removebg.js
@@ -3,36 +3,52 @@ const fs = require('fs-extra');
 
 module.exports.config = {
   name: "removebg",
-  version: "1.0.",
-  hasPermssion: 0,
-  credits: "Mark Hitsuraan",
-  description: "enhance your photo ",
-  usePrefix: true,
-  commandCategory: "image",
-  usages: "< reply image >",
-  cooldowns: 2,
+  version: "2.7",
+  hasPermission: 0,
+  credits: "Hazeyy",
+  description: "( 𝚁𝚎𝚖𝚘𝚟𝚎 𝙱𝚊𝚌𝚔𝚐𝚛𝚘𝚞𝚗𝚍 )",
+  commandCategory: "𝚗𝚘 𝚙𝚛𝚎𝚏𝚒𝚡",
+  usages: "( 𝚁𝚎𝚖𝚘𝚟𝚎 𝙱𝚊𝚌𝚔𝚐𝚛𝚘𝚞𝚗𝚍 𝙿𝚑𝚘𝚝𝚘 )",
+  cooldown: 3,
 };
 
-module.exports.run = async ({ api, event, args }) => {
-  let pathie = __dirname + `/cache/remove_bg.jpg`;
-  const { threadID, messageID } = event;
-
-  var mark = event.messageReply.attachments[0].url || args.join(" ");
-
+module.exports.handleEvent = async function ({ api, event }) {
   try {
-    api.sendMessage("Removing background...", threadID, messageID);
-    const response = await axios.get(`https://allinoneapis-0isy.onrender.com/api/try/removebg?url=${encodeURIComponent(mark)}`);
-    const processedImageURL = response.data.image_data;
+    if (!(event.body.indexOf("removebg") === 0 || event.body.indexOf("Removebg") === 0)) return;
 
-    const img = (await axios.get(processedImageURL, { responseType: "arraybuffer"})).data;
+    const args = event.body.split(/\s+/);
+    args.shift();
 
-    fs.writeFileSync(pathie, Buffer.from(img, 'binary'));
+    let pathie = __dirname + `/cache/removed_bg.jpg`;
+    const { threadID, messageID } = event;
 
-    api.sendMessage({
-      body: "Processed Image",
-      attachment: fs.createReadStream(pathie)
-    }, threadID, () => fs.unlinkSync(pathie), messageID);
+    let photoUrl = event.messageReply ? event.messageReply.attachments[0].url : args.join(" ");
+
+    if (!photoUrl) {
+      api.sendMessage("📸 𝙿𝚕𝚎𝚊𝚜𝚎 𝚛𝚎𝚙𝚕𝚢 𝚝𝚘 𝚊 𝚙𝚑𝚘𝚝𝚘 𝚝𝚘 𝚙𝚛𝚘𝚌𝚎𝚜𝚜 𝚊𝚗𝚍 𝚛𝚎𝚖𝚘𝚟𝚎 𝚋𝚊𝚌𝚔𝚐𝚛𝚘𝚞𝚗𝚍𝚜.", threadID, messageID);
+      return;
+    }
+
+    api.sendMessage("🕟 | 𝚁𝚎𝚖𝚘𝚟𝚒𝚗𝚐 𝚋𝚊𝚌𝚔𝚐𝚛𝚘𝚞𝚗𝚍, 𝙿𝚕𝚎𝚊𝚜𝚎 𝚠𝚊𝚒𝚝...", threadID, async () => {
+      try {
+        const response = await axios.get(`https://haze-code-merge-0f8f4bbdea12.herokuapp.com/api/try/removebg?url=${encodeURIComponent(photoUrl)}`);
+        const processedImageURL = response.data.image_data;
+
+        const img = (await axios.get(processedImageURL, { responseType: "arraybuffer" })).data;
+
+        fs.writeFileSync(pathie, Buffer.from(img, 'binary'));
+
+        api.sendMessage({
+          body: "✨ 𝙷𝚎𝚛𝚎'𝚜 𝚢𝚘𝚞𝚛 𝚒𝚖𝚊𝚐𝚎 𝚠𝚒𝚝𝚑𝚘𝚞𝚝 𝚋𝚊𝚌𝚔𝚐𝚛𝚘𝚞𝚗𝚍",
+          attachment: fs.createReadStream(pathie)
+        }, threadID, () => fs.unlinkSync(pathie), messageID);
+      } catch (error) {
+        api.sendMessage(`🔴 𝙴𝚛𝚛𝚘𝚛 𝚙𝚛𝚘𝚌𝚎𝚜𝚜𝚒𝚗𝚐 𝚒𝚖𝚊𝚐𝚎: ${error}`, threadID, messageID);
+      }
+    });
   } catch (error) {
-    api.sendMessage(`Error processing image: ${error}`, threadID, messageID);
-  };
+    api.sendMessage(`𝖤𝗋𝗋𝗈𝗋: ${error.message}`, event.threadID, event.messageID);
+  }
 };
+
+module.exports.run = async function ({ api, event }) {};
