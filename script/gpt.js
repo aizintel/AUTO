84a96bae10a92a0104ceba7bbd943c1b01064f70
Maoribot <k1mazumi+705@yandex.com>
2024-06-18 15:27:51 +0800

    Create gpt.js

---
+++ script/gpt.js
@@ -0,0 +1,30 @@
+const { get } = require('axios');
+
+module.exports.config = {
+  name: 'gpt',
+  credits: "cliff",
+  version: '1.0.0',
+  role: 0,
+  aliases: ["Gpt"],
+  cooldown: 0,
+  hasPrefix: false,
+  usage: "",
+};
+
+module.exports.run = async function ({ api, event, args }) {
+  const question = args.join(' ');
+  function sendMessage(msg) {
+    api.sendMessage(msg, event.threadID, event.messageID);
+  }
+
+  const url = "https://hercai.onrender.com/v3/hercai";
+
+  if (!question) return sendMessage("Please provide a question.");
+
+  try {
+    const response = await get(`${url}?question=${encodeURIComponent(question)}`);
+    sendMessage(response.data.reply);
+  } catch (error) {
+    sendMessage("An error occurred: " + error.message);
+  }
+};
