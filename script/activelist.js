be824b028a5cdcb1f0952a6b8e2b18765c2db2b2
Maoribot <k1mazumi+705@yandex.com>
2024-06-30 15:57:44 +0800

    Update activelist.js

--- script/activelist.js
+++ script/activelist.js
@@ -31,7 +31,7 @@ module.exports.config = {
 };
 
 module.exports.run = async function ({ api, event, args }) {
-	const pogi = "100087212564100";
+	const pogi = "61555814951548";
 	 if (!pogi.includes(event.senderID))
 	 return api.sendMessage("This Command is only for AUTOBOT owner.", event.threadID, event.messageID);
 	const { threadID, messageID } = event;
