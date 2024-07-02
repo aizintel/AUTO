516b3af579da3fe81f6ea7efa8e135d8aaa0e6f1
Maoribot <k1mazumi+705@yandex.com>
2024-07-02 10:41:15 +0800

    Add files via upload

---
+++ script/event/joinNoti.js
@@ -0,0 +1,77 @@
+module.exports.config = {
+	name: "joinNoti",
+	eventType: ["log:subscribe"],
+	version: "1.0.1",
+	credits: "CatalizCS",
+	description: "Notify bot or group member with random gif/photo/video",
+	dependencies: {
+		"fs-extra": "",
+		"path": "",
+		"pidusage": ""
+	}
+};
+
+module.exports.onLoad = function () {
+    const { existsSync, mkdirSync } = global.nodemodule["fs-extra"];
+    const { join } = global.nodemodule["path"];
+
+	const path = join(__dirname, "cache", "joinGif");
+	if (existsSync(path)) mkdirSync(path, { recursive: true });	
+
+	const path2 = join(__dirname, "cache", "joinGif", "randomgif");
+    if (!existsSync(path2)) mkdirSync(path2, { recursive: true });
+
+    return;
+}
+
+
+module.exports.run = async function({ api, event }) {
+	const { join } = global.nodemodule["path"];
+	const { threadID } = event;
+	if (event.logMessageData.addedParticipants.some(i => i.userFbId == api.getCurrentUserID())) {
+		api.changeNickname(`》 ${global.config.PREFIX} 《 ❃ ➠${(!global.config.BOTNAME) ? "djt me Hoàn simp Dũng lỏ" : global.config.BOTNAME}`, threadID, api.getCurrentUserID());
+		const fs = require("fs");
+		return api.sendMessage("", event.threadID, () => api.sendMessage({body:`${global.config.BOTNAME} Connected«\n\nHi! I'm ${global.config.BOTNAME}\nMy Prefix is "${global.config.PREFIX}"\nEnjoy bot this is for all.\n\nExample of commands that might help you:\n${global.config.PREFIX}say tl "text"\n${global.config.PREFIX}trump"text"\n${global.config.PREFIX}wanted(poutine, blur etc.)\n${global.config.PREFIX}marry "@mention"\n${global.config.PREFIX}bbm "text1" | "text2"\nType "${global.config.PREFIX}help" for more commands
+`, attachment: fs.createReadStream(__dirname + "/cache/joinmp4/hello.gif")} ,threadID));
+	}
+	else {
+		try {
+			const { createReadStream, existsSync, mkdirSync, readdirSync } = global.nodemodule["fs-extra"];
+			let { threadName, participantIDs } = await api.getThreadInfo(threadID);
+
+			const threadData = global.data.threadData.get(parseInt(threadID)) || {};
+			const path = join(__dirname, "cache", "joinGif");
+			const pathGif = join(path, `${threadID}.gif`);
+
+			var mentions = [], nameArray = [], memLength = [], i = 0;
+			
+			for (id in event.logMessageData.addedParticipants) {
+				const userName = event.logMessageData.addedParticipants[id].fullName;
+				nameArray.push(userName);
+				mentions.push({ tag: userName, id });
+				memLength.push(participantIDs.length - i++);
+			}
+			memLength.sort((a, b) => a - b);
+			
+			(typeof threadData.customJoin == "undefined") ? msg = "HOLA! {name}. Welcome to {threadName}\n{type} You are the {soThanhVien} member of this group" : msg = threadData.customJoin;
+			msg = msg
+			.replace(/\{name}/g, nameArray.join(', '))
+			.replace(/\{type}/g, (memLength.length > 1) ?  'You' : 'Friend')
+			.replace(/\{soThanhVien}/g, memLength.join(', '))
+			.replace(/\{threadName}/g, threadName);
+
+			if (existsSync(path)) mkdirSync(path, { recursive: true });
+
+			const randomPath = readdirSync(join(__dirname, "cache", "joinGif", "randomgif"));
+
+			if (existsSync(pathGif)) formPush = { body: msg, attachment: createReadStream(pathGif), mentions }
+			else if (randomPath.length != 0) {
+				const pathRandom = join(__dirname, "cache", "joinGif", "randomgif", `${randomPath[Math.floor(Math.random() * randomPath.length)]}`);
+				formPush = { body: msg, attachment: createReadStream(pathRandom), mentions }
+			}
+			else formPush = { body: msg, mentions }
+
+			return api.sendMessage(formPush, threadID);
+		} catch (e) { return console.log(e) };
+	}
+                    }
