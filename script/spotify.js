986be3f9fd9ed5dd14e1b10f24b9edde98dc80a8
Maoribot <k1mazumi+705@yandex.com>
2024-07-02 10:47:28 +0800

    Update spotify.js

--- script/spotify.js
+++ script/spotify.js
@@ -1,58 +1,52 @@
-let path = __dirname + "/cache/spotify.mp3";
-const axios = require("axios");
-const fs = require("fs");
+const axios = require('axios');
+const fs = require('fs');
 
 module.exports.config = {
 		name: "spotify",
-		version: "1.0.2",
+		version: "1.0.0",
 		role: 0,
-		credits: "joshua deku",
-		description: "Play and Download music from Spotify",
+		credits: "Jonell Magallanes",
+		description: "Search and play music from Spotify", //api by jonell Magallanes cc project
 		hasPrefix: false,
-		cooldown: 5,
-		aliases: ["spotify"]
+		usages: "[song name]",
+		cooldown: 10
 };
 
 module.exports.run = async function ({ api, event, args }) {
+		const listensearch = encodeURIComponent(args.join(" "));
+		const apiUrl = `https://jonellccapisproject-e1a0d0d91186.herokuapp.com/api/spotify?search=prompt=${listensearch}`;
+
+		if (!listensearch) return api.sendMessage("Please provide the name of the song you want to search.", event.threadID, event.messageID);
+
 		try {
-				const { spotify, spotifydl } = require("betabotz-tools");
-				let q = args.join(" ");
-				if (!q) return api.sendMessage("[ ❗ ] - Missing title of the song", event.threadID, event.messageID);
+				api.sendMessage("🎵 | Searching for your music on Spotify. Please wait...", event.threadID, event.messageID);
 
-				api.sendMessage("[ 🔍 ] Searching for “" + q + "” ...", event.threadID, async (err, info) => {
-						try {
-								const r = await axios.get("https://lyrist.vercel.app/api/" + q);
-								const { lyrics, title } = r.data;
-								const results = await spotify(encodeURI(q));
+				const response = await axios.get(apiUrl);
+				const { platform, status, data } = response.data;
 
-								let url = results.result.data[0].url;
+				if (status && platform === "Spotify") {
+						const { title, audio } = data;
 
-								const result1 = await spotifydl(url);
 
-								const dl = (
-										await axios.get(result1.result, { responseType: "arraybuffer" })
-								).data;
-								fs.writeFileSync(path, Buffer.from(dl, "utf-8"));
-								api.sendMessage(
-										{
-												body:
-														"·•———[ SPOTIFY DL ]———•·\n\n" + "Title: " + title + "\nLyrics:\n\n" +
-														lyrics +
-														"\n\nYou can download this audio by clicking this link or paste it to your browser: " +
-														result1.result,
-												attachment: fs.createReadStream(path),
-										},
-										event.threadID,
-										(err, info) => {
-												fs.unlinkSync(path);
-										}
-								);
-						} catch (error) {
-								console.error(error);
-								api.sendMessage("An error occurred while processing your request.", event.threadID);
-						}
-				});
-		} catch (s) {
-				api.sendMessage(s.message, event.threadID);
+						const filePath = `${__dirname}/cache/${Date.now()}.mp3`;
+						const writeStream = fs.createWriteStream(filePath);
+
+
+						const audioResponse = await axios.get(audio, { responseType: 'stream' });
+						audioResponse.data.pipe(writeStream);
+
+						writeStream.on('finish', () => {
+
+								api.sendMessage({
+										body: `🎧 Here's your music from Spotify enjoy listening\n\nTitle:${title}\n\n💿 Now Playing...`,
+										attachment: fs.createReadStream(filePath)
+								}, event.threadID);
+						});
+				} else {
+						api.sendMessage("❓ | Sorry, couldn't find the requested music on Spotify.", event.threadID);
+				}
+		} catch (error) {
+				console.error(error);
+				api.sendMessage("🚧 | An error occurred while processing your request.", event.threadID);
 		}
 };
