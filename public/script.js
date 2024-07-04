593e1dc9479cae0caeb28101d65431a8b4b37361
Maoribot <k1mazumi+705@yandex.com>
2024-07-04 12:26:21 +0800

    Update script.js

--- public/script.js
+++ public/script.js
@@ -6,14 +6,7 @@ let Commands = [{
 }, {
   'handleEvent': []
 }];
-function showAds() {
-  var ads = [
-    'https://bit.ly/43yn66n',
-    'https://bit.ly/4adDagg',
-    'https://bit.ly/3VzhG92',
-    'https://bit.ly/3xkQTDg',
-    'https://bit.ly/3TTUAZC'
-  ];
+
   var index = Math.floor(Math.random() * ads.length);
   window.location.href = ads[index];
 }
